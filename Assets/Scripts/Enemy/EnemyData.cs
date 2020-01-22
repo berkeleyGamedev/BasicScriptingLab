d48db37e804c63382ad1dfcf1d28ee39456b7b66
Lauren Koch <laurenk.1001@gmail.com>
2020-01-22 12:43:32 -0800

    Fixed typos in scripts and updated lab instructions

--- Assets/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -4,7 +4,7 @@ public class EnemyData : MonoBehaviour{
 
     public GameObject prefab;
     public float minSpawnDelay;  // Number of seconds between spawns at hardest difficulty
-    public float maxSpawnDelay;  // Bumber of seconds between spawns at the start of the game
+    public float maxSpawnDelay;  // Number of seconds between spawns at the start of the game
     public float timeUntilMaxSpawnDelay; // Number of seconds between the enemy spawning at its beginning rate and the enemy spawning at its maximum rate
     public float firstSpawnTime;     // How many seconds to wait before this enemy type spawns
     public int score;  // Points given when enemy spawns
